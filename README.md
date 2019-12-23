77dcb5a5c80c1f339623742b808426a226f5406a
Christian Meinert <cmeinert@gmx.net>
2019-12-23 11:14:24 +0100

    its christmas time

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # esp-christmas-lights
 control and dim led bipolar Christmas lights with an H-bridge
+
+## idea and nodes
+Christmas Lighs nowerdays often come with a little micro bringing 8 effects. Most of them are crap (why the hell are all dimming effects go down to zero - who likes a dark tree? even for some milliseconds?). If you are lucky the have a memory function (either with an eeprom or build in the micro) and you can have them "always on" even after power off.
+
+But what is most annoying is that there is no simple dimming function. So lets do this
+
+## the task
+2100 (3x700) LEDs are much to bright even for a bigger tree. My LEDs are too dense wired that I can take one or two stings out.
+So control the internat h-bridge or add a new one to dim the bipolar LED strings down. I had to go for a separate h-bridge because the drivers nowadays have the h-bridge and eeprom included in one controller. I`m using one of the L298 (working up to 36V! 31V is needed here) modules and an wemos-d1-mini and the job is done. The module is intended for DC motors so there are smoothing capacitors included which destroys the sharp signal... something an electronic engineer can perhaps fix. This limits the functionality and sometimes gives a flicker in certain settings but dimming in the needed range works very nicely.
+
+## how it is done
+I used the homie [convention esp8266 lib in version 3.0.1](https://homieiot.github.io/homie-esp8266/docs/develop-v3/quickstart/what-is-it/). It took a wile to get used to it (mostly because the docs are a little bit outdated) but in the end a quick and dirty source code is done. A weird mixture of hardware pwm and software switching the polarity. It certainly needs improvement as it sometimes misses the beat (WIFI, mqtt ... takes time) but works as needed
+
+It ended that 2100 LEDs are running on 3-6W power instead 18W before - nice.
+
+## pictures
+![one version with external h-bridge](./img/IMG_1071.jpg)
+one version with external h-bridge
+![one with internal h-bridge and without eeprom. The memory seams to be included in the micro](./img/IMG_1072.jpg)
+one with internal h-bridge and without eeprom. The memory seams to be included in the micro
+![ttl-timing seams perfect](./img/IMG_1073.jpg)
+ttl-timing seams perfect
+![but not always](./img/IMG_1075.jpg)
+but not always, see the delay on the second output
+![not perfect but working](./img/IMG_1077.jpg)
+not perfect but working
+![simple flow thanks to the homie convention](./img/IMG_1079.jpg)
+simple flow thanks to the homie convention.
+
+## Node-RED
+
+you need minimum of version 0.0.6 of the homie-convention node in order to work properly (using boolean together with "button as a switch")
+
+[here you find the flow](./Node-RED/flow.json)
+
+## the firmware
+
+everything is included in the repo. Contact me if you need any help.
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,141 @@
 #include <Arduino.h>
+#include <Homie.h>
+
+#define firmwareVersion "1.0.0";
+
+#define ENA 5
+#define IN1 4
+#define IN2 2
+
+int dimmer1=50;
+int dimmer2=50;
+int delayMs=5;
+
+boolean ledSwitch=false;
+boolean pinState=false;
+
+HomieNode lightNode("light", "Light", "switch");
+
+bool lightOnHandler(const HomieRange& range, const String& value) {
+  if (value != "true" && value != "false") return false;
+
+  ledSwitch = (value == "true");
+
+  lightNode.setProperty("on").send(value);
+  Homie.getLogger() << "Light is " << (ledSwitch ? "on" : "off") << endl;
+
+  return true;
+}
+
+bool dimmerHandler(const HomieRange& range, const String& value) {
+  for (byte i = 0; i < value.length(); i++) {
+    if (isDigit(value.charAt(i)) == false) return false;
+  }
+  dimmer1 = value.toInt();
+  if (dimmer1 > 1023) dimmer1=1023;
+  dimmer2 = dimmer1;
+  //digitalWrite(PIN_RELAY, on ? HIGH : LOW);
+  lightNode.setProperty("dimmer").send(value);
+  lightNode.setProperty("dimmer1").send(value);
+  lightNode.setProperty("dimmer2").send(value);
+  Homie.getLogger() << "Dimmer is " << (dimmer1) << endl;
+
+  return true;
+}
+
+bool dimmer1Handler(const HomieRange& range, const String& value) {
+  for (byte i = 0; i < value.length(); i++) {
+    if (isDigit(value.charAt(i)) == false) return false;
+  }
+  dimmer1 = value.toInt();
+  if (dimmer1 > 1023) dimmer1=1023;
+  //digitalWrite(PIN_RELAY, on ? HIGH : LOW);
+  lightNode.setProperty("dimmer1").send(value);
+  Homie.getLogger() << "Dimmer1 is " << (dimmer1) << endl;
+
+  return true;
+}
+
+
+bool dimmer2Handler(const HomieRange& range, const String& value) {
+  for (byte i = 0; i < value.length(); i++) {
+    if (isDigit(value.charAt(i)) == false) return false;
+  }
+  dimmer2 = value.toInt();
+  if (dimmer2 > 1023) dimmer2=1023;
+  //digitalWrite(PIN_RELAY, on ? HIGH : LOW);
+  lightNode.setProperty("dimmer2").send(value);
+  Homie.getLogger() << "Dimmer2 is " << (dimmer2) << endl;
+
+  return true;
+}
+
+bool delayHandler(const HomieRange& range, const String& value) {
+  for (byte i = 0; i < value.length(); i++) {
+    if (isDigit(value.charAt(i)) == false) return false;
+  }
+  delayMs = value.toInt();
+  if (delayMs > 10) delayMs=10;
+  //digitalWrite(PIN_RELAY, on ? HIGH : LOW);
+  lightNode.setProperty("delay").send(value);
+  Homie.getLogger() << "delay is " << (delayMs) << endl;
+
+  return true;
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  Serial.begin(115200);
+  Serial << endl << endl;
+  Homie_setBrand("Christmas-Tree");
+  Homie_setFirmware("Christmas", "0.0.1"); // The underscore is not a typo! See Magic bytes
+  lightNode.advertise("on").settable(lightOnHandler);
+  lightNode.advertise("on").setName("Switch");
+  lightNode.advertise("on").setDatatype("boolean");
+
+  lightNode.advertise("dimmer").settable(dimmerHandler);
+  lightNode.advertise("dimmer").setName("Dimmer");
+  lightNode.advertise("dimmer").setDatatype("integer");
+  lightNode.advertise("dimmer").setFormat("0:1023");
+
+  lightNode.advertise("dimmer1").settable(dimmer1Handler);
+  lightNode.advertise("dimmer1").setName("Dimmer-1");
+  lightNode.advertise("dimmer1").setDatatype("integer");
+  lightNode.advertise("dimmer1").setFormat("0:1023");
+
+  lightNode.advertise("dimmer2").settable(dimmer2Handler);
+  lightNode.advertise("dimmer2").setName("Dimmer-2");
+  lightNode.advertise("dimmer2").setDatatype("integer");
+  lightNode.advertise("dimmer2").setFormat("0:1023");
+
+  lightNode.advertise("delay").settable(delayHandler);
+  lightNode.advertise("delay").setName("Delay");
+  lightNode.advertise("delay").setDatatype("integer");
+  lightNode.advertise("delay").setFormat("0:10");
+
+  Homie.setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Homie.loop();
+  if (ledSwitch) {
+    if (pinState) {
+      analogWrite(ENA, dimmer1);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+    } else {
+      analogWrite(ENA, dimmer2);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+    }
+    pinState=!pinState;
+    delay(delayMs);
+  } else {
+    analogWrite(ENA, 0);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }
 }
